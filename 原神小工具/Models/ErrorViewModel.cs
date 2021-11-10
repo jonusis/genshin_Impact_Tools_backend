706068aac9dacd3cf2a5a2c93129938885588b7c
Apple <apple@AppledeMacBook-Pro.local>
2021-11-10 19:51:33 +0800

    feat(genshin): feat new project

---
+++ 原神小工具/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace 原神小工具.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
